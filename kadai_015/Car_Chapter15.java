3032f1a611617e4d7a4e9e026cbfe2a428b22683
tamao21 <tt21komasan@icloud.com>
2025-08-04 23:08:08 +0900

    Update Car_Chapter15.java

--- kadai_015/Car_Chapter15.java
+++ kadai_015/Car_Chapter15.java
@@ -4,7 +4,25 @@ public class Car_Chapter15 {
 	private int gear =1;
 	private int speed =10;
 	public void changeGear(int afterGear){
-		 gear = afterGear;
+		gear = afterGear;
+		switch(gear){
+		case 1  -> {
+			System.out.println("ギアを" + gear +"に変更しました"); speed = 10;
+		}
+		case 2  -> {
+			System.out.println("ギアを" + gear +"に変更しました"); speed = 20;
+		}        
+		case 3  -> {
+			System.out.println("ギアを" + gear +"に変更しました"); speed = 30;
+		}
+		case 4  -> {
+			System.out.println("ギアを" + gear +"に変更しました"); speed = 40;
+		}
+		case 5  -> {
+			System.out.println("ギアを" + gear +"に変更しました"); speed = 50;
+		}
+		}
+		 
 	}
 	public void run(){
 		 System.out.println("時速" + speed +"km");
