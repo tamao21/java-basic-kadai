b23c6ca19fee2c9f9e1606668b7027b0ee7ab1ca
tamao21 <tt21komasan@icloud.com>
2025-08-04 23:22:14 +0900

    Update Car_Chapter15.java

--- kadai_015/Car_Chapter15.java
+++ kadai_015/Car_Chapter15.java
@@ -4,28 +4,31 @@ public class Car_Chapter15 {
 	private int gear =1;
 	private int speed =10;
 	public void changeGear(int afterGear){
-		gear = afterGear;
-		switch(gear){
+		
+		switch(afterGear){
 		case 1  -> {
-			System.out.println("ギアを" + gear +"に変更しました"); speed = 10;
+			System.out.println("ギア"+ gear + "から"+ afterGear + "に切り替えました"); speed = 10;
 		}
 		case 2  -> {
-			System.out.println("ギアを" + gear +"に変更しました"); speed = 20;
+			System.out.println("ギア"+ gear + "から"+ afterGear + "に切り替えました"); speed = 20;
 		}        
 		case 3  -> {
-			System.out.println("ギアを" + gear +"に変更しました"); speed = 30;
+			System.out.println("ギア"+ gear + "から"+ afterGear + "に切り替えました"); speed = 30;
 		}
 		case 4  -> {
-			System.out.println("ギアを" + gear +"に変更しました"); speed = 40;
+			System.out.println("ギア"+ gear + "から"+ afterGear + "に切り替えました"); speed = 40;
 		}
 		case 5  -> {
-			System.out.println("ギアを" + gear +"に変更しました"); speed = 50;
+			System.out.println("ギア"+ gear + "から"+ afterGear + "に切り替えました"); speed = 50;
+		}
+		default -> {
+			System.out.println("ギア"+ gear + "から"+ afterGear+ "に切り替えました"); speed = 10;
 		}
 		}
 		 
 	}
 	public void run(){
-		 System.out.println("時速" + speed +"km");
+		 System.out.println("速度は時速" + speed +"kmです");
 	}
 
 }
