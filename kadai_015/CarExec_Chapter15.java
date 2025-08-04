453f88d8e4a9022d56a08eab09bba925b4eb0e08
tamao21 <tt21komasan@icloud.com>
2025-08-04 23:07:48 +0900

    Update CarExec_Chapter15.java

--- kadai_015/CarExec_Chapter15.java
+++ kadai_015/CarExec_Chapter15.java
@@ -3,8 +3,9 @@ package kadai_015;
 public class CarExec_Chapter15 {
 
 	public static void main(String[] args) {
-		changeGear(3);
-		run();
+		Car_Chapter15 car = new Car_Chapter15();
+		car.changeGear(3);
+		car.run();
 	}
 
 }
