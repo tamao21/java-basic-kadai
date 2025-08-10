e8e5a9d1bdebe2ebab9e9251134457cc9f9121a1
tamao21 <tt21komasan@icloud.com>
2025-08-10 11:27:44 +0900

    Add files via upload

---
+++ kadai_018/KatoExec_Chapter18.java
@@ -0,0 +1,19 @@
+package kadai_018;
+
+public class KatoExec_Chapter18 {
+
+	public static void main(String[] args) {
+		KatoHanako_Chapter18 hanako = new KatoHanako_Chapter18();
+		KatoIchiro_Chapter18 ichiro = new KatoIchiro_Chapter18();
+		KatoIchiro_Chapter18 taro = new KatoIchiro_Chapter18();
+		
+//		taro.setGivenName();
+//		ichiro.setGivenName();
+//		hanako.setGivenName();
+		
+		taro.execIntroduce();
+		ichiro.execIntroduce();
+		hanako.execIntroduce();
+	}
+
+}
---
+++ kadai_018/Kato_Chapter18.java
@@ -0,0 +1,17 @@
+package kadai_018;
+
+abstract public class Kato_Chapter18 {
+	String familyName = "加藤";
+	String givenName;
+	String adress= "東京都中野区〇×";
+	public void commonIntroduce() {
+		System.out.println("名前は"+ familyName + givenName +"です");
+		System.out.println("住所は"+ adress +"です");
+	}
+	abstract public void eachIntroduce();
+	
+	public void execIntroduce() {
+		commonIntroduce();
+		eachIntroduce();
+	}
+}
